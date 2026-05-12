09aa08a0ee07cc80dd32ab559998f7b4af2f5161
Victor <vgarciacr.inf@upsa.es>
2026-05-12 12:43:40 +0200

    despliegue fase 2 finalizado y comprobado

---
+++ gestion-eventos/f2-kubernetes-cloud/deploy-cloud.sh
@@ -0,0 +1,19 @@
+gcloud container clusters resize cluster-gestion-eventos --num-nodes=1 --zone europe-southwest1-a
+
+mvn clean package
+
+docker build -f ws-catalogo/src/main/docker/Dockerfile.jvm -t gestion-eventos-ws-catalogo:latest ws-catalogo
+docker build -f ws-reservas/src/main/docker/Dockerfile.jvm -t gestion-eventos-ws-reservas:latest ws-reservas
+
+docker tag gestion-eventos-ws-catalogo:latest europe-southwest1-docker.pkg.dev/gestionreservaseventos/repo-eventos/ws-catalogo:v1
+docker tag gestion-eventos-ws-reservas:latest europe-southwest1-docker.pkg.dev/gestionreservaseventos/repo-eventos/ws-reservas:v1
+
+docker push europe-southwest1-docker.pkg.dev/gestionreservaseventos/repo-eventos/ws-catalogo:v1
+docker push europe-southwest1-docker.pkg.dev/gestionreservaseventos/repo-eventos/ws-reservas:v1
+
+gcloud container clusters get-credentials cluster-gestion-eventos --region europe-southwest1-a
+
+kubectl apply -f f2-kubernetes-cloud/ws-catalogo.yaml
+kubectl apply -f f2-kubernetes-cloud/ws-reservas.yaml
+
+kubectl get pods -w
